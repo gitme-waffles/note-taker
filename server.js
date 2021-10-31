2d86ffef5c005ccd87d8e809cad2b1b712fcb58d
dossAnania <85494162+dossAnania@users.noreply.github.com>
2021-10-31 21:36:45 +1100

    p-lock.json and working port

--- server.js
+++ server.js
@@ -5,3 +5,9 @@ const app = express();
 const PORT = 3001; // port to use, uncoment when needed
 
 app.use(express.static('public'));
+
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public/notes.html')))
+
+app.listen(PORT, () => {
+    console.log('Listening on PORT 3001');
+})
