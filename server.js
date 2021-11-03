6845d4192b16ba8cafea0c7a00b06c4c94af0447
dossAnania <85494162+dossAnania@users.noreply.github.com>
2021-11-03 20:48:57 +1100

    added routes  to landing page, notes and api

--- server.js
+++ server.js
@@ -1,13 +1,27 @@
-const express = require('express')
-const path = require('path');
+const express = require("express");
+const path = require("path");
+const api = require("./routes/index");
+// const notes = require("./db/db.json");
 
 const app = express();
 const PORT = 3001; // port to use, uncoment when needed
 
-app.use(express.static('public'));
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 
-app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public/notes.html')))
+
+app.use("/api", api);
+// all routes that are in api, are prefixed with /api
+app.use(express.static("public"));
+
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "public/index.html"))
+);
+
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "public/notes.html"))
+);
 
 app.listen(PORT, () => {
-    console.log('Listening on PORT 3001');
-})
+  console.log(`Listening on PORT ${PORT}`);
+});
